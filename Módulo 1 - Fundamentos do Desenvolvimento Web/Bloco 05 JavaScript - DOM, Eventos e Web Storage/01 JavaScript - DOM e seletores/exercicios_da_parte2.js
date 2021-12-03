9bef560fbfe68426ef53d65fdbb280c22e10b31c
Daniel <danieldanpbcarvalho4@gmail.com>
2021-12-03 19:25:45 -0300

    Exercicios do course

---
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 05 JavaScript - DOM, Eventos e Web Storage/01 JavaScript - DOM e seletores/exercicios_da_parte2.js
@@ -0,0 +1,10 @@
+(document.getElementById('page-title').innerText = 'Em ritmo de fuga');
+(document.getElementById('subtitle').innerText = 'Eu amo o filme Em ritmo de fuga');
+(document.getElementById('second-paragraph'));
+
+const paragraph = document.getElementById("paragraph");
+	paragraph.style.color = "red";
+
+const secondParagraph = document.getElementById('second-paragraph');
+    secondParagraph.style.color = "green";
+    secondParagraph.style.fontFamily = "Verdana"
